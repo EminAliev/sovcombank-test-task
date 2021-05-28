c29e7614af4cc5e524f195df37fe46f6b3becd5b
emin <samburer200@gmail.com>
2021-05-28 20:13:39 +0300

    add log model

--- table/models.py
+++ table/models.py
@@ -20,10 +20,10 @@ class Data(models.Model):
     product = models.CharField('Продукт', max_length=20, choices=PRODUCT_CHOICES)
     phone_number = models.CharField('Номер телефона', validators=[RegexValidator(
         regex='^[0-9]{10}$',
-        message='Номер телефона должен быть в формате 0000000000'
+        message='Номер телефона должен быть в формате 9xxxxxxxxx'
     )],
                                     max_length=10,
-                                    help_text='Номер телефона в формате 0000000000')
+                                    help_text='Номер телефона в формате 9xxxxxxxxx')
     solution = models.CharField('Решение', choices=SOLUTION_CHOICES, max_length=20, blank=True, null=True)
     comment = models.TextField('Комментарий', blank=True, null=True)
 
@@ -33,3 +33,38 @@ class Data(models.Model):
 
     def __str__(self):
         return self.product
+
+
+class ChangeLog(models.Model):
+    PRODUCT_CHOICES = [
+        ('', 'Выберите тип продукта'),
+        ('car', 'Авто'),
+        ('consumer', 'Потреб'),
+        ('deposit', 'Залог'),
+        ('mortgage', 'Ипотека'),
+    ]
+    SOLUTION_CHOICES = [
+        ('', 'Решение по заявке'),
+        ('approved', 'Одобрено'),
+        ('denied', 'Отказано'),
+        ('temp_denied', 'Временно отказано'),
+    ]
+    data_id = models.IntegerField("ID заявки")
+    date = models.DateTimeField('Дата заявки', auto_now_add=True)
+    product = models.CharField('Продукт', max_length=20, choices=PRODUCT_CHOICES)
+    phone_number = models.CharField('Номер телефона', validators=[RegexValidator(
+        regex='^[0-9]{10}$',
+        message='Номер телефона должен быть в формате 9xxxxxxxxx'
+    )],
+                                    max_length=10,
+                                    help_text='Номер телефона в формате 9xxxxxxxxx')
+    solution = models.CharField('Решение', choices=SOLUTION_CHOICES, max_length=20, blank=True, null=True)
+    comment = models.TextField('Комментарий', blank=True, null=True)
+
+    class Meta:
+        ordering = ('date',)
+        verbose_name = 'Логи'
+        verbose_name_plural = 'Логи'
+
+    def __str__(self):
+        return f'{self.id} - {self.phone_number}'
